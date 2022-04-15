cc54784c5d62767796c4d0b96c15b6df3f6aab98
a1elfino <102170808+a1elfino@users.noreply.github.com>
2022-04-15 19:36:03 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,10 +6,13 @@
 </head>
 
 <body>
-    <?php
-    $retour = mail('ayoub2000elfinou@gmail.com', 'Envoi depuis la page Contact', $_POST['Message'], '');
-    if ($retour)
+<?php
+if(isset($_POST['Message'])){
+    $retour = mail('ayoub2000elfinou@gmail.com', 'Envoi depuis la page Contact', $_POST['Message'], "Reply-to:" . $_POST['Votre adresse e-mail']);
+    if ($retour){
         echo '<p>Votre message a bien été envoyé.</p>';
-    ?>
+    }
+}
+?>
 </body>
 </html>
