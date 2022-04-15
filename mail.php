1f8d468ccdff07feb7205f669bdc598b69b07bff
a1elfino <102170808+a1elfino@users.noreply.github.com>
2022-04-15 20:18:03 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,17 +6,10 @@
 </head>
 
 <body>
-	<?php
-		if(isset($_POST['Message'])){
-			$message = "Ce message vous a été envoyé via la page du site Portfolio-Ayoub-Elfinou
-				Prenom : " . $-POST["Votre prénom"] ."
-				e-mail : " . $-POST["Votre adresse e-mail"] ."
-				Message : " . $-POST["Message"];
-  			$retour = mail("ayoub2000elfinou@gmail.com", $Message, "From:https://a1elfino.github.io/Portfolio-Ayoub-Elfinou/ . "\r\n" . "Reply-to: . $_POST['Votre adresse e-mail']);
-    		if ($retour){
-        		echo '<p>Votre message a bien été envoyé.</p>';
-  			  }
-		}
-	?>
+    <?php
+    $retour = mail('ayoub2000elfinou@gmail.com', 'Envoi depuis la page Contact', $_POST['Message'], "Replay-to:" .$_POST['Votre adresse e-mail'] ;
+    if ($retour)
+        echo '<p>Votre message a bien été envoyé.</p>';
+    ?>
 </body>
 </html>
